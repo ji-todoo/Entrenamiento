9850d67daecdd6a2ca564386335381ff01e1b24f
andres <ji@todoo.co>
2020-02-20 12:41:45 +0000

    "borrar"

--- prueba/models/.ipynb_checkpoints/prueba1-checkpoint.py
+++ prueba/models/.ipynb_checkpoints/prueba1-checkpoint.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields
 
+class prueba1(models.Model):
+     _name = 'prueba1.prueba1'
+     _description = 'prueba1.prueba1'
 
-# class prueba(models.Model):
-#     _name = 'prueba.prueba'
-#     _description = 'prueba.prueba'
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+     @api.depends('value')
+     def _value_pc(self):
+         for record in self:
+             record.value2 = float(record.value) / 100
--- prueba/models/prueba1.py
+++ prueba/models/prueba1.py
@@ -6,12 +6,12 @@ class prueba1(models.Model):
      _name = 'prueba1.prueba1'
      _description = 'prueba1.prueba1'
 
-     nombre = fields.Char()
-     valor = fields.Integer()
-     direccion = fields.Float(compute="_value_pc", store=True)
-     descripcion = fields.Text()
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
 
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+     @api.depends('value')
+     def _value_pc(self):
+         for record in self:
+             record.value2 = float(record.value) / 100
